b41ec4a3cf15acd436cd0073ddc0e67017c55048
Vertmo <basile.pesin@gmail.com>
2018-06-05 10:08:52 +0200

    opam and ocamlfind integration

---
+++ lib/src/oContrainte.mli
@@ -0,0 +1,15 @@
+(*****************************************************************************************)
+(* OContrainte : A constraint programming library, written with microcontrollers in mind *)
+(*                                                                                       *)
+(*                                    Basile Pesin                                       *)
+(*                                                                                       *)
+(*                  Licenced under GNU General Public License v3.0                       *)
+(*****************************************************************************************)
+
+(** Main module *)
+
+module Domain = OCont_domain
+module Variable = OCont_variable
+module Expression = OCont_expression
+module Constraint = OCont_constraint
+module Solver = OCont_solver
