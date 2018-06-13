7cdab451d60b144690074293e316fdec588040a5
Vertmo <basile.pesin@gmail.com>
2018-06-13 17:54:18 +0200

    Got rid of mutually recursive function in oCont_solver

--- lib/src/oCont_solver.ml
+++ lib/src/oCont_solver.ml
@@ -3,9 +3,12 @@ open OCont_constraint
 open OCont_domain
 
 let rec solve vars constrs = match vars with
-  | t::q -> if (isAssigned t)
-    then solve q constrs
-    else chooseValue (asList (domain t)) t q constrs
+  | t::q when (isAssigned t) -> solve q constrs
+  | t::q -> let reus = List.exists (fun v -> assign t v;
+                                     (areConsistent constrs) && (solve vars constrs)) (asList (domain t)) in
+    if reus then true else (unassign t; false)
+    (*then solve q constrs
+      else chooseValue (asList (domain t)) t q constrs*)
   | [] -> areConsistent constrs
 
 and chooseValue values var vars constrs = match values with
