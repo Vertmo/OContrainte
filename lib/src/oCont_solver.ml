40b95d13d4573ff9de0d469f2e62d366d08e92e9
Vertmo <basile.pesin@gmail.com>
2018-06-14 14:59:42 +0200

    What ?

--- lib/src/oCont_solver.ml
+++ lib/src/oCont_solver.ml
@@ -2,15 +2,13 @@ open OCont_variable
 open OCont_constraint
 open OCont_domain
 
-let rec solve vars constrs = match vars with
-  | t::q when (isAssigned t) -> solve q constrs
+let propagate vars constrs = true
+
+let rec backtrack vars constrs = match vars with
+  | t::q when (isAssigned t) -> backtrack q constrs
   | t::q -> let reus = List.exists (fun v -> assign t v;
-                                     (areConsistent constrs) && (solve vars constrs)) (asList (domain t)) in
+                                     (areConsistent constrs) && (backtrack vars constrs)) (asList (domain t)) in
     if reus then true else (unassign t; false)
-    (*then solve q constrs
-      else chooseValue (asList (domain t)) t q constrs*)
   | [] -> areConsistent constrs
 
-and chooseValue values var vars constrs = match values with
-  | [] -> unassign var; false
-  | t::q -> assign var t; ((areConsistent constrs) && (solve vars constrs)) || (chooseValue q var vars constrs)
+let solve vars constrs = backtrack vars constrs
