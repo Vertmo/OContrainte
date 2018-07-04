daa3d7cdb8f4c1b0eda3194363bf31a3d61e2ad8
Vertmo <basile.pesin@gmail.com>
2018-07-04 16:46:04 +0200

    Improved order operators

--- lib/src/oCont_operators.ml
+++ lib/src/oCont_operators.ml
@@ -20,28 +20,36 @@ let (~<>) = {
 
 let (~<) = {
   f = (<);
-  propagate = fun (d1, d2) ->
-    (filter (fun v -> exists (fun e -> v < e) d2) d1,
-     filter (fun v -> exists (fun e -> e < v) d1) d2);
+  propagate = fun (d1, d2) -> match (min d1, max d2) with
+    | (Some n1, Some n2) ->
+      (filter (fun v -> v < n2) d1,
+       filter (fun v -> n1 < v) d2)
+    | _ -> (empty, empty)
 }
 
 let (~>) = {
   f = (>);
-  propagate = fun (d1, d2) ->
-    (filter (fun v -> exists (fun e -> v > e) d2) d1,
-     filter (fun v -> exists (fun e -> e > v) d1) d2);
+  propagate = fun (d1, d2) -> match (max d1, min d2) with
+    | (Some n1, Some n2) ->
+      (filter (fun v -> v > n2) d1,
+       filter (fun v -> n1 > v) d2)
+    | _ -> (empty, empty)
 }
 
 let (~<=) = {
   f = (<=);
-  propagate = fun (d1, d2) ->
-    (filter (fun v -> exists (fun e -> v <= e) d2) d1,
-     filter (fun v -> exists (fun e -> e <= v) d1) d2);
+  propagate = fun (d1, d2) -> match (min d1, max d2) with
+    | (Some n1, Some n2) ->
+      (filter (fun v -> v <= n2) d1,
+       filter (fun v -> n1 <= v) d2)
+    | _ -> (empty, empty)
 }
 
 let (~>=) = {
   f = (>=);
-  propagate = fun (d1, d2) ->
-    (filter (fun v -> exists (fun e -> v >= e) d2) d1,
-     filter (fun v -> exists (fun e -> e >= v) d1) d2);
+  propagate = fun (d1, d2) -> match (max d1, min d2) with
+    | (Some n1, Some n2) ->
+      (filter (fun v -> v >= n2) d1,
+       filter (fun v -> n1 >= v) d2)
+    | _ -> (empty, empty)
 }
--- tests/operatorsTest.ml
+++ tests/operatorsTest.ml
@@ -10,20 +10,20 @@ let testPropagateEq test_ctxt =
   assert_equal (asList dom1) [1;2]
 
 
-let testPropagateSup test_ctxt =
+let testPropagateSupEq test_ctxt =
   let dom1 = (range 1 7) and dom2 = fromList [3;4;9] in
-  let (dom1, dom2) = (~>).propagate (dom1, dom2) in
-  assert_equal (asList dom1) [4;5;6];
+  let (dom1, dom2) = (~>=).propagate (dom1, dom2) in
+  assert_equal (asList dom1) [3;4;5;6];
   assert_equal (asList dom2) [3;4]
 
-let testPropagateInfEq test_ctxt =
+let testPropagateInf test_ctxt =
   let dom1 = (range 1 7) and dom2 = fromList [0;3;4] in
-  let (dom1, dom2) = (~<=).propagate (dom1, dom2) in
-  assert_equal (asList dom1) [1;2;3;4];
+  let (dom1, dom2) = (~<).propagate (dom1, dom2) in
+  assert_equal (asList dom1) [1;2;3];
   assert_equal (asList dom2) [3;4]
 
 let suite = [
   "propagateEq">::testPropagateEq;
-  "propagateSup">::testPropagateSup;
-  "propagateInfEq">::testPropagateInfEq;
+  "propagateSupEq">::testPropagateSupEq;
+  "propagateInf">::testPropagateInf;
 ]
